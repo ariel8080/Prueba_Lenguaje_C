939c710dce583e7b5194440179886464121d35e1
riverae8080 <riverae8080@yahoo.com.ar>
2020-09-25 02:52:31 +0000

    Initial commit

---
+++ alumno.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int func() {
+  printf("Hello3 World\n");
+  printf("Hello5 World\n");
+  return 0;
+}
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello1 World\n");
+  printf("Hello2 World\n");
+  func();
+  return 0;
+}
